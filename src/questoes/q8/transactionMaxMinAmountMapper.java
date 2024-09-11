3700cdb82e958a4a69a47cd5be336bcda54e2383
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2024-09-11 20:16:07 -0300

    [Q8] STARTED

---
+++ src/questoes/q8/transactionMaxMinAmountMapper.java
@@ -0,0 +1,29 @@
+package questoes.q8;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class transactionMaxMinAmountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    // Função utilizada para o Mapeamento dos dados.
+    public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+        String line = value.toString();
+        String columnHeadName = "country_or_area";
+        Boolean isColumnHead = line.startsWith(columnHeadName);
+        String[] columnsArray = line.split(";"); // Transformar/dividr os valores da linha atual em Array.
+
+        String countryValue = columnsArray[0];
+        Boolean isNotBrazilData = !countryValue.equals("Brazil");
+
+        if (isColumnHead || isNotBrazilData) return;
+
+        String yearValue = columnsArray[1];
+
+        if (yearValue.equals("2016")) {
+            Text outputKey = new Text(columnsArray[0]);
+
+            con.write(outputKey, new LongWritable(Long.parseLong(columnsArray[5])));
+        }
+    }
+}
